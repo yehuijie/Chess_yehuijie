6235944481fb72810adf116afedcad7fa673490a
yehuijie <133075209+yehuijie@users.noreply.github.com>
2024-05-02 00:11:20 +0200

    commentato

--- Source/Chess_yehuijie/Public/BasePiece.h
+++ Source/Chess_yehuijie/Public/BasePiece.h
@@ -6,8 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "BasePiece.generated.h"
 
+// Enumerator class thah helps keeping track of which type of Piece we're reffering to
 UENUM()
-enum class EPiece : uint8
+enum class EPiece : uint8 
 {
 	Pawn,
 	Rook,
@@ -18,6 +19,7 @@ enum class EPiece : uint8
 	None,
 };
 
+// Enumerator class that keeps track of whether a Piece was Clicked by a Player or not
 UENUM()
 enum class EPieceStatus : uint8
 {
@@ -25,6 +27,7 @@ enum class EPieceStatus : uint8
 	NotClicked,
 };
 
+// Enumerator class that keeps track of whether the suggested Moves of the CLicked Piece are Spawned or not
 UENUM()
 enum class EPieceMoves : uint8
 {
@@ -32,22 +35,15 @@ enum class EPieceMoves : uint8
 	NotSpawned,
 };
 
-//UENUM()
-//enum class EPieceColor : uint8
-//{
-//        White,
-//        Black,
-//	None,
-//};
-
+// Enumerator class that keeps track of whether a Piece is in a Position that could cause it to be Eaten by another Piece
 UENUM()
 enum class EPieceToEat : uint8
 {
 	ToBeEaten,
-	NotToBeEaten,
-	
+	NotToBeEaten,	
 };
 
+// Enumerator class that keeps track of whether a Piece is currently on the Board or not
 UENUM()
 enum class EPieceOnBoard : uint8
 {
@@ -55,6 +51,7 @@ enum class EPieceOnBoard : uint8
 	OnBoard,
 };
 
+// Declaration of ABasePiece Class, which inherits from AActor
 UCLASS()
 class CHESS_YEHUIJIE_API ABasePiece : public AActor
 {
@@ -64,30 +61,31 @@ public:
 	// Sets default values for this actor's properties
 	ABasePiece();
 
-	// set the (x, y) position
+	// Set and Get the (x, y) Position
 	void SetBoardPosition(const double InX, const double InY);
-
-	// get the (x, y) position
 	FVector2D GetBoardPosition();
 
+        // Set and Get the Type of the Piece (Pawn, Rook, etc)
 	void SetPiece(const EPiece PieceType);
 	EPiece GetPiece();
 
+        // Set and Get Clicked or NotClicked Piece
 	void SetPieceStatus(const EPieceStatus Status);
 	EPieceStatus GetPieceStatus();
 
-        //void SetPieceColor(const EPieceColor Color);
-        //EPieceColor GetPieceColor();
-
-	void SetOldPosition(const double InX, const double InY);
+	// Set and Get Current (x,y) Position of a Piece that is moving (current Position will be the Old Position after moving)
+        void SetOldPosition(const double InX, const double InY);
 	FVector2D GetOldPosition();
 
+        // Set and Get whether Movement Tiles are Spawned or not
 	void SetPieceMoves(const EPieceMoves Moves);
 	EPieceMoves GetPieceMoves();
 
-	void SetPieceToEat(const EPieceToEat ToBeEaten);
+	// Set and Get whether a Piece is a threatened Position or not
+        void SetPieceToEat(const EPieceToEat ToBeEaten);
 	EPieceToEat GetPieceToEat();
 
+        // Set and Get whether a Piece is currently on Board or not
 	void SetIsPieceOnBoard(const EPieceOnBoard OnBoard);
 	EPieceOnBoard GetIsPieceOnBoard();
 
@@ -101,36 +99,39 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* StaticMeshComponent;
 
-	// (x, y) position of the tile
+	// (x, y) Position of the Piece
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	FVector2D BoardPosition;
 
+        // (x,y) Current Position of the Piece
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	FVector2D OldPosition;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	// Type of the Piece
+        UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	EPiece Piece;
 
+        // Clicked/NotClicked
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	EPieceStatus PieceStatus;
 
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	//EPieceColor PieceColor;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	// if the Movement Tiler are Spawned or Not
+        UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	EPieceMoves PieceMoves;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	// is the Piece possibly getting eaten
+        UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	EPieceToEat PieceToEat;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	// whether or not the Piece is Currently on the Board
+        UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	EPieceOnBoard IsPieceOnBoard;
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// destroy a base sign actor
+	// funzione copia-incollata dal TTT, è rimasta perche non ho fatto in tempo a levarla quando ero in Lab e non volevo compromettere in alcun modo il codice
 	UFUNCTION()
 	void SelfDestroy();
 
